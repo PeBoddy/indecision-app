83b7f28ba2935faeb5752cd22316c40f8269817a
Pascal Decker <p-dizzle@Pascals-MBP.fritz.box>
2018-08-26 22:45:58 +0200

    some config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+    const isProd = env === 'production';
+    // const CSSExtract = new ExtractTextPlugin('styles.scss');
+
+    console.log('env', env);
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                // use: CSSExtract.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                // })
+            }]
+        },
+        // plugins: [
+        //     CSSExtract
+        // ],
+        devtool: isProd ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
     }
 };
